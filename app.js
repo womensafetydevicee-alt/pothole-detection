94496a94ffefc5987e514cd09680b93b0b00167c
womensafetydevicee-alt <womensafetydevicee@gmail.com>
2026-08-26 16:39:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // wss://xxxxxxxx.s1.eu.hivemq.cloud:8884/mqtt
 
 const MQTT_HOST =
-    "9e3ed49b452c4e20bf321f93db6bb743.s1.eu.hivemq.cloud";
+    "ws://9e3ed49b452c4e20bf321f93db6bb743.s1.eu.hivemq.cloud:884/mqtt";
 
 
 const MQTT_USERNAME =
